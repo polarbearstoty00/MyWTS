a7cae14c385c6e56b41ca81813f87e204940ce65
polarbearstoty00 <polarbearstoty00@gmail.com>
2025-03-18 17:23:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,10 @@ if st.session_state["page"] == "main":
         # 보유 종목을 표 형태로 변환하여 표시
         if balance_details:
             df = pd.DataFrame(balance_details)
-            df = df.reset_index(drop=True)
-            st.dataframe(df)
+            df = df.set_index("hname")  # 종목명을 인덱스로 설정
+            df = df[["tappamt", "tdtsunik"]]  # 평가금액과 평가손익만 표시
+            df = df.rename(columns={"hname" : "종목명", "tappamt": "평가금액", "tdtsunik": "평가손익"})  # 컬럼명 변경
+            st.table(df)
         else:
             st.write("보유 종목이 없습니다.")
     except Exception as e:
