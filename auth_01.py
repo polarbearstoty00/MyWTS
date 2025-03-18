7556dc00b345b9e5e7797ebac86a80119a536a10
polarbearstoty00 <polarbearstoty00@gmail.com>
2025-03-18 18:46:15 +0900

    Update auth_01.py

--- auth_01.py
+++ auth_01.py
@@ -54,23 +54,19 @@ def get_account_balance(access_token):
 
 
 # 계좌 전체 수익률, 예수금 등을 조회
-def get_account_balance(access_token):
-    """LS증권 API에서 계좌 잔고를 조회하는 함수"""
+def get_account_summary(access_token):
+    """LS증권 API에서 계좌 전체 수익률, 예수금 등을 조회하는 함수"""
     headers = {
         "Content-Type": "application/json; charset=UTF-8",
         "Authorization": f"Bearer {access_token}",
-        "tr_cd": "t0424",
+        "tr_cd": "CSPAQ12200",
         "tr_cont": "N",
         "tr_cont_key": ""
     }
     
     body = json.dumps({
-        "t0424InBlock": {
-            "prcgb": "",
-            "chegb": "",
-            "dangb": "",
-            "charge": "",
-            "cts_expcode": ""
+        "CSPAQ12200InBlock1": {
+            "BalCreTp": "0"
         }
     })
     
@@ -78,13 +74,13 @@ def get_account_balance(access_token):
     
     # 응답 JSON 출력
     try:
-        account_json = response.json()
-        st.write("잔고 조회 응답 데이터:", account_json)  # Streamlit UI에서 응답 데이터 확인
+        account_summary_json = response.json()
+        st.write("응답 데이터:", account_summary_json)  # Streamlit UI에서 응답 데이터 확인
 
-        if "t0424OutBlock" in account_json and "t0424OutBlock1" in account_json:
-            return account_json["t0424OutBlock"], account_json["t0424OutBlock1"]
+        if "CSPAQ12200OutBlock2" in account_summary_json:
+            return account_summary_json["CSPAQ12200OutBlock2"][0]
         else:
-            raise Exception("계좌 잔고 데이터를 찾을 수 없습니다.")
+            raise Exception("계좌 요약 정보를 찾을 수 없습니다.")
     
     except json.JSONDecodeError:
         raise Exception(f"JSON 디코딩 오류 발생: {response.text}")
